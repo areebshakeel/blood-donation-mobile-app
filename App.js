33ef7420be0935eb54ab13b32bac6c5ef9ddfb2f
unknown <areebshakeel>
2020-10-15 03:14:26 +0500

    Login Component

--- App.js
+++ App.js
@@ -34,101 +34,30 @@ const App: () => React$Node = () => {
   const [password, setPassword] = useState();
   console.log(password);
   console.log(email);
- 
+
   return (
-    <View
-      style={{
-        flex: 1,
-        flexDirection: 'column',
-        alignItems: 'center',
-        justifyContent: 'space-evenly',
-      }}>
+    <View style={styles.container}>
       <View>
-        <Image style={{height: 180, width: 150}} source={Logo} />
-      </View>
-
-      <View>
-        <Text
-          style={{fontSize: 30, textShadowColor: 'red', textShadowRadius: 5}}>
-          SIGN IN
-        </Text>
-      </View>
-
-      <View
-        style={{
-          flex: 1,
-          flexDirection: 'column',
-          justifyContent: 'space-between',
-          width: 350,
-          padding: 8,
-          // borderColor: 'black',
-          // borderTopWidth: 2,
-          // borderBottomWidth: 2,
-          // borderLeftWidth: 2,
-          // borderRightWidth: 2,
-        }}>
         <View>
-          <TextInput
-            style={{borderBottomWidth: 1, borderColor: 'grey'}}
-            placeholder="User Name"
-            autoCompleteType='email'
-            onChangeText={(value)=>setEmail(value)}
-
-          />
+          <Image source={Logo} style={styles.logo} />
         </View>
         <View>
-          <TextInput
-            secureTextEntry={true}
-            onChangeText={(value) => setPassword(value)}
-            style={{borderBottomWidth: 1, borderColor: 'grey'}}
-            placeholder="Password"
-          />
-        </View>
-        <View
-          style={{
-            flex: 1,
-            flexDirection: 'row',
-            
-            marginTop:30
-          }}>
-          <View>
-            <CheckBox></CheckBox>
-          </View>
-          <View>
-            <Text>I agree to the terms and services</Text>
-          </View>
-          <View style={{marginTop:50}}>
-        <TouchableOpacity style={styles.button}>
-          <Text style={{color:'white'}}>Login</Text>
-        </TouchableOpacity>
-        </View>
-        </View>
-        
-
-        <View>
-          <Text style={{marginTop:20}}>Forgot Password?</Text>
+          <Text>SIGN IN</Text>
         </View>
       </View>
-
-      
     </View>
   );
 };
 
 const styles = StyleSheet.create({
-  button:{
-    alignItems: "center",
-    backgroundColor: "red",
-    borderRadius:10,
-    padding: 10
+  container: {
+    flex: 1,
+    flexDirection: 'column',
   },
-  facebook:{
-    alignItems: "center",
-    backgroundColor: "blue",
-    borderRadius:10,
-
-  },
-  inputs: {
+  logo: {
+    width: 200,
+    height: 250,
+    borderTopWidth: 10,
     borderColor: 'black',
   },
   scrollView: {
